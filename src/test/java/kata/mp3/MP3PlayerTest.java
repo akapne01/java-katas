76e033c0909ced9e430823c9d7381d0ff5a5ee40
Agnese Kapnere <agnese.kapnere@ig.com>
2019-07-24 12:00:22 +0100

    Adding mp3 until AC 4

--- src/test/java/kata/mp3/MP3PlayerTest.java
+++ src/test/java/kata/mp3/MP3PlayerTest.java
@@ -4,20 +4,45 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.contains;
 
 public class MP3PlayerTest {
 
-    @Test
-    public void shouldFindByTitle() throws Exception {
-        MP3Player mp3Player = new MP3Player();
+   @Test
+   public void shouldReturnSongWhenSearched() {
+      // Given
+      MP3Player mp3Player = new MP3Player();
+      Song song = new Song("Random", "Randomizer");
+      mp3Player.addSong(song);
 
-        Song song = new Song("Should I Stay or Should I Go", "The Clash");
-        mp3Player.addSong(song);
-        List<Song> matchingSongs = mp3Player.findByTitle(song.getTitle());
+      // When
+      Song returnedSong = mp3Player.findByTitle("Random").get(0);
 
-        assertThat(matchingSongs.size(), is(1));
-        assertThat(matchingSongs.get(0), is(song));
-    }
+      // Then
+      assertThat(returnedSong, is(song));
+
+   }
+
+   @Test
+   public void shouldReturnSongWhenPartialSearch() {
+      // Given
+      MP3Player mp3Player = new MP3Player();
+      Song song1 = new Song("Random", "Randomizer");
+      Song song2 = new Song("Run ran", "Randomizer");
+      Song song3 = new Song("Sun", "Randomizer");
+      mp3Player.addSong(song1);
+      mp3Player.addSong(song2);
+      mp3Player.addSong(song3);
+
+      // When
+      List<Song> returedList = mp3Player.findByTitle("ran");
+
+      // Then
+      assertThat(returedList.size(), is(2));
+      assertTrue(returedList.contains(song1));
+      assertTrue(returedList.contains(song2));
+   }
 }
