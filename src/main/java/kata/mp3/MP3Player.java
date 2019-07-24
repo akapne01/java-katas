a0a9eb182bc80bc2e543b68d2d8922578b9ea939
Agnese Kapnere <agnese.kapnere@ig.com>
2019-07-24 11:58:54 +0100

    Adding mp3 until AC 4

--- src/main/java/kata/mp3/MP3Player.java
+++ src/main/java/kata/mp3/MP3Player.java
@@ -1,17 +1,51 @@
 package kata.mp3;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class MP3Player {
+   private final List<Song> songList = new ArrayList<>();
 
-    public void addSong(Song song) {
+   public void addSong(Song song) {
+      songList.add(song);
+   }
 
-    }
+   public List<Song> findByTitle(String songTitle) {
 
-    public List<Song> findByTitle(String songTitle) {
-        return new ArrayList<>();
-    }
+      return filterWithPredicate(Song::getTitle, songTitle);
 
+//      List<Song> matchingSongs = new ArrayList<>();
+//
+//      for (Song song : songList) {
+//         if (song.getTitle().toLowerCase().contains(songTitle.toLowerCase())) {
+//            matchingSongs.add(song);
+//         }
+//      }
+//
+//      return matchingSongs;
+   }
 
+   public List<Song> findByArtist(String artistNme) {
+      return filterWithPredicate(Song::getArtist, artistNme);
+
+//      List<Song> matchingSongs = new ArrayList<>();
+//
+//      for (Song song : songList) {
+//         if (song.getArtist().toLowerCase().contains(artistNme.toLowerCase())) {
+//            matchingSongs.add(song);
+//         }
+//      }
+//
+//      return matchingSongs;
+   }
+
+   private List<Song> filterWithPredicate(Function<Song, String> function, String searchTerm) {
+      return songList.stream()
+            .filter(song -> function.apply(song).toLowerCase().contains(searchTerm.toLowerCase()))
+            .collect(Collectors.toList());
+   }
 }
