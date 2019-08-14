5146351d4fa5de09bca0ebbbfd191a26f0c5a2f7
Agnese Kapnere <agnese.kapnere@ig.com>
2019-08-14 17:53:35 +0100

    Finish surfing

--- src/main/java/kata/surfing/SurfingApp.java
+++ src/main/java/kata/surfing/SurfingApp.java
@@ -1,30 +1,60 @@
 package kata.surfing;
 
-import kata.amazon.Notifier;
+import java.util.List;
 
 public class SurfingApp {
 
    private final static int WAIKIKI_CODE = 55;
    private final static String GO_AHEAD = "You should go surfing today";
-   private final static String ASAP = "Go surfing ASAP!";
+   private final static String ASAP = "Go surfing ASAP";
    private final WeatherApp weatherApp;
    private final NotifierApp notifierApp;
    private final ClockApp clockApp;
+   private final GPSApp gpsApp;
 
-   public SurfingApp(WeatherApp weatherApp, NotifierApp notifierApp, ClockApp clockApp) {
+   public SurfingApp(WeatherApp weatherApp, NotifierApp notifierApp, ClockApp clockApp, GPSApp gpsApp) {
       this.weatherApp = weatherApp;
       this.notifierApp = notifierApp;
       this.clockApp = clockApp;
+      this.gpsApp = gpsApp;
    }
 
+   /**
+    * This method is called every minute by some other method outside.
+    */
    public void alertOnGoodWeatherConditions() {
-      WeatherConditions weatherConditions = weatherApp.getWeatherConditions(WAIKIKI_CODE);
+
+      final int minutes = clockApp.getCurrentTime().getMinute();
+      if (minutes != 0) {
+         return;
+      }
+
+      List<City> citiesNearBy = gpsApp.getCitiesNearby();
+
+      citiesNearBy.stream()
+         .filter(city -> city.hasBeach())
+         .forEach(city -> notifyIfShouldGoSurfing(city));
+
+//      for (City city : citiesNearBy) {
+//
+//         boolean cityHasBeach = city.hasBeach();
+//         if (!cityHasBeach) {
+//            continue;
+//         }
+//
+//         notifyIfShouldGoSurfing(city);
+//      }
+   }
+
+   private void notifyIfShouldGoSurfing(City city) {
+      WeatherConditions weatherConditions = weatherApp.getWeatherConditions(city.getCityId());
       final int temperature = weatherConditions.getTemperatureInCelsius();
       final int windSpeed = weatherConditions.getWindSpeedInMetersPerHour();
+
       if (temperature >= 25 && windSpeed >= 12) {
-         notifierApp.notifyAndVibrate(ASAP);
+         notifierApp.notifyAndVibrate(ASAP + " to " + city.getCityName() + "!");
       } else if ((temperature >= 21 && windSpeed >= 12) || (temperature >= 25 && windSpeed >= 9)) {
-         notifierApp.notify(GO_AHEAD);
+         notifierApp.notify(GO_AHEAD + " to " + city.getCityName());
       }
    }
 }
--- src/test/java/kata/surfing/SurfingAppTest.java
+++ src/test/java/kata/surfing/SurfingAppTest.java
@@ -1,26 +1,39 @@
 package kata.surfing;
 
-import kata.cocktailbar.Clock;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockingDetails;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 public class SurfingAppTest {
 
+   private static final String GOOD_WEATHER_WAIKIKI_MESSAGE = "You should go surfing today to Waikiki";
+   private static final String AMAZING_WEATHER_WAIKIKI_MESSAGE = "Go surfing ASAP to Waikiki!";
+   private final ClockApp clockApp = mock(ClockApp.class);
+   private final WeatherApp weatherApp = mock(WeatherApp.class);
+   private final NotifierApp notifierApp = mock(NotifierApp.class);
+   private final GPSApp gpsApp = mock(GPSApp.class);
+   private final SurfingApp surfingApp = new SurfingApp(weatherApp, notifierApp, clockApp, gpsApp);
+
+   @Before
+   public void setUp() throws Exception {
+      when(clockApp.getCurrentTime()).thenReturn(LocalDateTime.of(LocalDate.now(), LocalTime.of(15, 00)));
+      List<City> citiesNearBy = new ArrayList<>();
+      citiesNearBy.add(new City(55, "Waikiki", true));
+      when(gpsApp.getCitiesNearby()).thenReturn(citiesNearBy);
+   }
+
    @Test
    public void shouldAlertOnGoodWeatherConditions() {
-      WeatherApp weatherApp = mock(WeatherApp.class);
-      NotifierApp notifierApp = mock(NotifierApp.class);
-      ClockApp clockApp = mock(ClockApp.class);
-      SurfingApp surfingApp = new SurfingApp(weatherApp, notifierApp, clockApp);
 
       // Given -> weather conditions are good
       WeatherConditions goodWeatherConditions = new WeatherConditions(21, 12);
@@ -30,15 +43,11 @@ public class SurfingAppTest {
       surfingApp.alertOnGoodWeatherConditions();
 
       // Then -> notification received
-      verify(notifierApp).notify("You should go surfing today");
+      verify(notifierApp).notify(GOOD_WEATHER_WAIKIKI_MESSAGE);
    }
 
    @Test
    public void shouldAlertOnDifferentGoodWeatherConditions() {
-      WeatherApp weatherApp = mock(WeatherApp.class);
-      NotifierApp notifierApp = mock(NotifierApp.class);
-      ClockApp clockApp = mock(ClockApp.class);
-      SurfingApp surfingApp = new SurfingApp(weatherApp, notifierApp, clockApp);
 
       // Given -> weather conditions are good
       WeatherConditions goodWeatherConditions = new WeatherConditions(25, 9);
@@ -48,19 +57,15 @@ public class SurfingAppTest {
       surfingApp.alertOnGoodWeatherConditions();
 
       // Then -> notification received
-      verify(notifierApp).notify("You should go surfing today");
+      verify(notifierApp).notify(GOOD_WEATHER_WAIKIKI_MESSAGE);
    }
 
    @Test
    public void shouldNotNotifyWhenBadWeatherConditions() {
-      WeatherApp weatherApp = mock(WeatherApp.class);
-      NotifierApp notifierApp = mock(NotifierApp.class);
-      ClockApp clockApp = mock(ClockApp.class);
-      SurfingApp surfingApp = new SurfingApp(weatherApp, notifierApp, clockApp);
 
       // Given -> weather conditions are good
-      WeatherConditions goodWeatherConditions = new WeatherConditions(1, 9);
-      when(weatherApp.getWeatherConditions(55)).thenReturn(goodWeatherConditions);
+      WeatherConditions badWeatherConditions = new WeatherConditions(1, 9);
+      when(weatherApp.getWeatherConditions(55)).thenReturn(badWeatherConditions);
 
       // When -> check weather conditions
       surfingApp.alertOnGoodWeatherConditions();
@@ -71,10 +76,6 @@ public class SurfingAppTest {
 
    @Test
    public void shouldNotifyAndVibrateWhenGreatWeatherConditions() {
-      WeatherApp weatherApp = mock(WeatherApp.class);
-      NotifierApp notifierApp = mock(NotifierApp.class);
-      ClockApp clockApp = mock(ClockApp.class);
-      SurfingApp surfingApp = new SurfingApp(weatherApp, notifierApp, clockApp);
 
       // Given -> weather conditions are good
       WeatherConditions greatWeatherConditions = new WeatherConditions(25, 12);
@@ -84,16 +85,11 @@ public class SurfingAppTest {
       surfingApp.alertOnGoodWeatherConditions();
 
       // Then -> notification received
-      verify(notifierApp).notifyAndVibrate("Go surfing ASAP!");
+      verify(notifierApp).notifyAndVibrate(AMAZING_WEATHER_WAIKIKI_MESSAGE);
    }
 
    @Test
    public void shouldNotifyOnFullHourWhenGoodWeather() {
-      WeatherApp weatherApp = mock(WeatherApp.class);
-      NotifierApp notifierApp = mock(NotifierApp.class);
-      ClockApp clockApp = mock(ClockApp.class);
-      SurfingApp surfingApp = new SurfingApp(weatherApp, notifierApp, clockApp);
-
 
       // Given -> weather conditions are good
       WeatherConditions greatWeatherConditions = new WeatherConditions(25, 12);
@@ -104,16 +100,11 @@ public class SurfingAppTest {
       surfingApp.alertOnGoodWeatherConditions();
 
       // Then -> notification received
-      verify(notifierApp).notifyAndVibrate("Go surfing ASAP!");
+      verify(notifierApp).notifyAndVibrate(AMAZING_WEATHER_WAIKIKI_MESSAGE);
    }
 
    @Test
    public void shouldNotNotifyIfNotFullHourWhenGoodWeather() {
-      WeatherApp weatherApp = mock(WeatherApp.class);
-      NotifierApp notifierApp = mock(NotifierApp.class);
-      ClockApp clockApp = mock(ClockApp.class);
-      SurfingApp surfingApp = new SurfingApp(weatherApp, notifierApp, clockApp);
-
 
       // Given -> weather conditions are good
       WeatherConditions greatWeatherConditions = new WeatherConditions(25, 12);
@@ -127,4 +118,19 @@ public class SurfingAppTest {
       verifyZeroInteractions(notifierApp);
    }
 
+   @Test
+   public void shouldNotifyWhenNearbyCitiesHasGoodWeatherAndBeach() {
+      // Given -> Has a beach and weather conditions are good
+      WeatherConditions goodWeatherConditions = new WeatherConditions(25, 9);
+      when(weatherApp.getWeatherConditions(9)).thenReturn(goodWeatherConditions);
+      List<City> citiesNearBy = new ArrayList<>();
+      citiesNearBy.add(new City(9, "Riga", true));
+      when(gpsApp.getCitiesNearby()).thenReturn(citiesNearBy);
+
+      // When -> When check weather conditions in nearby cities
+      surfingApp.alertOnGoodWeatherConditions();
+
+      // Then -> notification received with city name
+      verify(notifierApp).notify("You should go surfing today to Riga");
+   }
 }
